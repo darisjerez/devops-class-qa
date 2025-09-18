c3c8cbbb291b55c75d23a0b13327d8bfe87fef18
Gabriel Eduardo Asensio Peralta <gabriel.asensio@babelgroup.com>
2025-09-18 11:40:52 -0400

    Configura reporter JUnit directamente en wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,2 +1,35 @@
-import { config as shared } from './wdio.shared.js'
-export const config = { ...shared }
+export const config = {
+    runner: 'local',
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    maxInstances: 1,
+    capabilities: [{
+        maxInstances: 1,
+        browserName: 'chrome'
+    }],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: ['chromedriver'],
+
+    framework: 'cucumber',
+
+    reporters: [
+        'spec',
+        ['junit', {
+            outputDir: './reports',
+            outputFileFormat: function (options) {
+                return `results-${options.cid}.xml`
+            }
+        }]
+    ],
+
+    cucumberOpts: {
+        require: ['./test/step-definitions/*.js'],
+        timeout: 60000
+    }
+}
